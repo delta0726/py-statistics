f0170d375083ca5f9ea92fdf8741101cb1c28f32
Keido Sumi <delta0726@nta.pial.jp>
2022-05-03 08:08:43 +0900

    Update 3-1

--- book/new_stat_text/3-1-Pythonによる記述統計：1変量データ編.py
+++ book/new_stat_text/3-1-Pythonによる記述統計：1変量データ編.py
@@ -1,19 +1,21 @@
-# ***************************************************************************************
-# Title     : あたらしいPythonで学ぶ統計学の教科書
-# Chapter   : 3- Pythonによる記述統計：1変量データ編
-# Created by: Owner
-# Created on: 2021/4/15
-# Page      : P106 - P115
-# ***************************************************************************************
+# ***********************************************************************************************
+# Title   : あたらしいPythonで学ぶ統計学の教科書
+# Chapter : 3 Pythonによるデータ分析
+# Theme   : 1 Pythonによる記述統計：1変量データ編
+# Date    : 2022/05/03
+# Page    : P106 - P115
+# URL     : https://logics-of-blue.com/python-stats-book-support/
+# ***********************************************************************************************
 
 
 # ＜概要＞
 # - 1変量データの統計量の算出方法を確認
 
 
-# ＜ポイント＞
-# - 複数のライブラリで同じ処理を関数やメソッドとして実装しているケースがある
-# - 本書ではScipyを優先して使用する
+# ＜メモ＞
+# - 書籍はscipyで記述しているが、リタイア警告が出るためnumpyで記述
+#   --- sum/mean/sqrtなど当ページのほぼ全ての関数で警告が出現
+#   --- scipy.sum is deprecated and will be removed in SciPy 2.0.0, use numpy.sum instead
 
 
 # ＜目次＞
@@ -27,13 +29,15 @@
 
 # 0 準備 -------------------------------------------------------------------------------
 
+# ライブラリ
 import numpy as np
 import scipy as sp
 
 from scipy import stats
 
 
-# 1変量データの管理
+# データ定義
+# --- 1変量データの管理
 fish_data = np.array([2, 3, 3, 4, 4, 4, 4, 5, 5, 6])
 fish_data_2 = np.array([2, 3, 3, 4, 4, 4, 4, 5, 5, 100])
 fish_data_3 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -41,8 +45,13 @@ fish_data_3 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 # 1 合計/平均 -------------------------------------------------------------------------
 
+# ＜ポイント＞
+# - 複数のライブラリで同じ処理を関数やメソッドとして実装しているケースがある
+# - 本書ではScipyを優先して使用する
+# - サンプルサイズを求める関数はPytonの標準関数を使用
+
+
 # 合計
-sp.sum(fish_data)
 np.sum(fish_data)
 sum(fish_data)
 fish_data.sum()
@@ -53,56 +62,68 @@ len(fish_data)
 # 平均（期待値）
 # --- 計算プロセス
 N = len(fish_data)
-sum_value = sp.sum(fish_data)
+sum_value = np.sum(fish_data)
 mu = sum_value / N
 mu
 
 # 平均（期待値）
 # --- 関数による実装
-sp.mean(fish_data)
+np.mean(fish_data)
 
 
 # 2 分散/標準偏差 -------------------------------------------------------------------------
 
-# 計算部品
-mu = sp.sum(fish_data) / N
+# ＜ポイント＞
+# - 標本分散はNで除するが、不偏分散はN-1で除して計算する
+#   --- 標本分散は分散を過小に見積もるバイアスを持つ
+#   --- 不偏分散はN-1で割るので標本分散より少し大きくなる
+
+
+# 平均値
+# --- 分散を計算するためのパーツ
+mu = np.sum(fish_data) / N
 
 # 標本分散
-# --- 計算プロセス
-sigma_2_sample = sp.sum((fish_data - mu) ** 2) / N
+# --- 公式に基づいて計算
+sigma_2_sample = np.sum((fish_data - mu) ** 2) / N
 sigma_2_sample
 
 # 標本分散
-# --- 関数による実装
-# --- 標本分散の際はddofを0とする
-sp.var(fish_data, ddof=0)
+# --- 関数による計算
+np.var(fish_data, ddof=0)
+
 
 # 不偏分散
-# --- 標本分散の際はddofを1とする
-sigma_2 = sp.sum((fish_data - mu) ** 2) / (N - 1)
+# --- 公式に基づいて計算）
+sigma_2 = np.sum((fish_data - mu) ** 2) / (N - 1)
 sigma_2
 
 # 標本分散
-# --- 関数による実装
-# --- 標本分散の際はddofを1とする
-sp.var(fish_data, ddof=1)
+# --- 関数による計算
+np.var(fish_data, ddof=1)
 
 
-# 標準偏差
+# (不偏)標準偏差
 # --- 不偏分散から計算する
-sigma = sp.sqrt(sigma_2)
+sigma = np.sqrt(sigma_2)
 sigma
 
-# 標準偏差
+# (不偏)標準偏差
 # --- 関数による実装
-sp.std(fish_data, ddof=1)
+np.std(fish_data, ddof=1)
 
 
 # 3 標準化 ---------------------------------------------------------------------------------
 
-# 計算部品
-mu = sp.sum(fish_data) / N
-sigma = sp.std(fish_data, ddof=1)
+# ＜ポイント＞
+# - 標準化はデータ系列を平均を0で標準偏差を1にする変換のこと
+
+
+# 計算パーツ
+# --- 平均値
+# --- (不偏)標準偏差
+mu = np.sum(fish_data) / N
+sigma = np.std(fish_data, ddof=1)
 
 # 標準化
 # --- 計算プロセス
@@ -113,20 +134,20 @@ zscore
 # 4 その他の統計量 --------------------------------------------------------------------------
 
 # 最大値
-sp.amax(fish_data)
+np.amax(fish_data)
 
 # 最小値
-sp.amin(fish_data)
+np.amin(fish_data)
 
 # 中央値
-sp.median(fish_data)
+np.median(fish_data)
 
 # 中央値のロバスト性
 # --- 異常値を含むデータ
 # --- 平均値は異常値の影響を強く受ける
 # --- 中央値はロバスト
-sp.mean(fish_data_2)
-sp.median(fish_data_2)
+np.mean(fish_data_2)
+np.median(fish_data_2)
 
 
 # 5 scipy.statsと四分位点 ------------------------------------------------------------------
@@ -138,6 +159,3 @@ sp.median(fish_data_2)
 # パーセンタイルの値を取得
 stats.scoreatpercentile(fish_data_3, 25)
 stats.scoreatpercentile(fish_data_3, 75)
-
-# 参考：ライブラリ構成
-dir(stats)
